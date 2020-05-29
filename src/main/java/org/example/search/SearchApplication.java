1d9b9a877690e9b7b0fdeff8d72dd72c63d1a20f
Aleksey Filippov <al.filippov@ulstu.ru>
2020-05-29 08:16:09 +0400

    Initial commit

---
+++ src/main/java/org/example/search/SearchApplication.java
@@ -0,0 +1,18 @@
+package org.example.search;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("HW!");
+	}
+}
